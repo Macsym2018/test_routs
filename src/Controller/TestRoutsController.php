d3c41d06f43eab407562c47f472ad2f856437eb3
Maksym.Fedorchuk <maksym.fedorchuk@mfedorchuk-MacBook-Pro.local>
2023-07-14 18:29:37 +0300

    Add title arguments

--- src/Controller/TestRoutsController.php
+++ src/Controller/TestRoutsController.php
@@ -29,5 +29,20 @@ class TestRoutsController extends ControllerBase {
     return $build;
   }
 
+  /**
+   * A title callback method for test routes.
+   *
+   * @param array $_title_arguments
+   *   Optional array from the route defaults.
+   * @param string $_title
+   *   Optional _title string from the route defaults.
+   *
+   * @return string
+   *   The route title.
+   */
+  public function contentTitle(array $_title_arguments = [], $_title = '') {
+    return t($_title_arguments['title']);
+  }
+
 
 }
